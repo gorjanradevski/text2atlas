2915bcf5c49eabdc73dbf03fe902b1baa0d3bc75
gorjanradevski <gorjanradevski@gmail.com>
2019-12-08 16:21:09 +0100

    editted the naive baselines

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -3,45 +3,6 @@ import numpy as np
 from typing import List, Union
 
 
-def bbox_volume(bbox: np.ndarray):
-    """
-        Return
-        :param bbox: bounding box (3, 2)
-        :return: Volume of bounding box
-        """
-    return (bbox[:, 1] - bbox[:, 0]).prod()
-
-
-def bbox_intersection_volume(bboxes: List[np.ndarray]):
-    mins = np.empty((3, 0))
-    maxs = np.empty((3, 0))
-
-    for bbox in bboxes:
-        mins = np.concatenate((mins, bbox[:, 0][..., None]), axis=1)
-        maxs = np.concatenate((maxs, bbox[:, 1][..., None]), axis=1)
-
-    intersection = np.concatenate(
-        (mins.max(axis=1)[..., None], maxs.min(axis=1)[..., None]), axis=1
-    )
-
-    return max((float(bbox_volume(intersection))), 0.0)
-
-
-def bbox_inside(pred: np.ndarray, bboxes: np.ndarray):
-    """
-    Return
-    :param pred: prediction for one sample, shape (3,)
-    :param bboxes: set of bounding boxes of k organs inside the sample sentence, shape (k, 3, 2)
-    :return: Array with k entries
-     1.0 at i-th entry - pred is inside the i-th bounding box,
-     0.0 at i-th entry - prediction is outside of the i-th bounding box
-    """
-    corrects = (
-        np.concatenate((pred - bboxes[:, :, 0], bboxes[:, :, 1] - pred), axis=-1) >= 0
-    ).all(axis=-1)
-    return corrects.astype(float)
-
-
 def bbox_shrink(
     bboxes: Union[List[List], List[np.ndarray], np.ndarray], bbox_shrink: float = 0.0
 ) -> np.ndarray:
