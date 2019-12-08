3ee589d9cd2c307e3ce7d91e54851ecb138c4336
dusangrujicic <dusangr22@gmail.com>
2019-12-08 18:01:03 +0100

    update of naive baselines

--- src/naive_baseline_mapping.py
+++ src/naive_baseline_mapping.py
@@ -32,7 +32,7 @@ def bbox_intersection_volume(bboxes: List[np.ndarray]):
     return max((float(bbox_volume(intersection))), 0.0)
 
 
-def center_naive(organs_dir_path: str, train_samples, test_samples, mode: str):
+def frequency_naive(organs_dir_path: str, train_samples, test_samples, mode: str):
     # Prepare paths
     organ2mass_path = os.path.join(organs_dir_path, "organ2mass_new.json")
     organ2ind_path = os.path.join(organs_dir_path, "organ2ind_new.json")
@@ -63,85 +63,58 @@ def center_naive(organs_dir_path: str, train_samples, test_samples, mode: str):
         )
 
     print(
-        f"At {mode} mode the average probability of hitting: {evaluator.get_current_accuracy()}"
+        f"At {mode} mode the average probability of hitting: {100 * evaluator.get_current_accuracy()}%"
     )
     print(
         f"At {mode} mode the average distance to hit: {evaluator.get_current_distance()}"
     )
 
 
-def random_naive(organs_dir_path: str, samples, mode, bbox_shrink=0.0):
-    if mode == "random_easy":
-        organs_bbox = json.load(
-            open(os.path.join(organs_dir_path, "organ2bbox_new.json"))
-        )
-        bboxes = np.concatenate(
-            [np.array(box) for organ, box in organs_bbox.items()], axis=1
-        )
-        volume_box = np.concatenate(
-            (
-                bboxes[:, 0::2].min(axis=1)[..., None],
-                bboxes[:, 1::2].max(axis=1)[..., None],
-            ),
-            axis=1,
-        )
-    else:
-        body_hull = json.load((os.path.join(organs_dir_path, "body_hull.json")))
-        body_hull = np.array(body_hull["body_hull"])
-        volume_box = np.concatenate(
-            (body_hull.min(axis=0)[..., None], body_hull.max(axis=0)[..., None]), axis=1
-        )
+def random_naive(organs_dir_path: str, samples, mode):
+    organ2voxels = json.load(
+        open(os.path.join(organs_dir_path, "organ2voxels_new.json"))
+    )
+    ind2organ = json.load(
+        open(os.path.join(organs_dir_path, "ind2organ_new.json"))
+    )
+    body_hull = json.load(
+        open(os.path.join(organs_dir_path, "body_hull.json"))
+    )
+    body_hull = np.array(body_hull["body_hull"])
+    volume_box = np.concatenate(
+        (body_hull.min(axis=0)[..., None], body_hull.max(axis=0)[..., None]), axis=1
+    )
 
     volume = bbox_volume(volume_box)
     hit_probs = []
 
     for sample in tqdm(samples):
-        # bboxes = sample["bboxes"]
-        bboxes = sample["bboxes"]
-        bboxes = np.array(bboxes)
-        for bbox in bboxes:
-            dims = bbox[:, 1] - bbox[:, 0]
-            bbox[:, 0] += (bbox_shrink / 2) * dims
-            bbox[:, 1] -= (bbox_shrink / 2) * dims
-        """
-        CALCULATING AREA OF UNION OF CUBOIDS
-        FORMULA (V - volume, U - union)
-        sum(V(singles)) - sum(V(overlapping pairs)) + sum(V(overlapping triplets)) - sum(V(overlapping quads)) ...
-        We go to triplets, which should be approximately correct
-        """
         union_volume = 0.0
-        bboxes_seen = []
-        for bbox in bboxes:
-            bbox = np.array(bbox)
-            union_volume += bbox_volume(bbox)
-            for i, bbox_i in enumerate(bboxes_seen):
-                union_volume -= bbox_intersection_volume([bbox, bbox_i])
-                for j, bbox_j in enumerate(bboxes_seen[:i] + bboxes_seen[i + 1 :]):
-                    union_volume += bbox_intersection_volume([bbox, bbox_i, bbox_j])
-            bboxes_seen.append(bbox)
+        organ_indices = sample["organ_indices"]
+        for organ_index in organ_indices:
+            union_volume += len(organ2voxels[ind2organ[str(organ_index)]])
 
         hit_probs.append(union_volume / volume)
 
     print(
-        f"At blind guessing in {mode} mode, the average probability of hitting is: {00 * sum(hit_probs) / len(hit_probs)}"
+        f"At blind guessing in {mode} mode, the average probability of hitting is: {100 * sum(hit_probs) / len(hit_probs)}%"
     )
 
 
 def main():
     args = parse_args()
     assert args.mode in [
-        "center",
-        "random_easy",
-        "random_hard",
-    ], 'Parameter "difficulty" needs to be one of {"center", "random_easy", "random_hard"}'
+        "frequency",
+        "random",
+    ], 'Parameter "difficulty" needs to be one of {"frequency", "random"}'
 
-    if args.mode in ["random_easy", "random_hard"]:
+    if args.mode == "random":
         test_samples = json.load(open(args.test_samples_path))
-        random_naive(args.organs_dir_path, test_samples, args.mode, args.bbox_shrink)
+        random_naive(args.organs_dir_path, test_samples, args.mode)
     else:
         train_samples = json.load(open(args.train_samples_path))
         test_samples = json.load(open(args.test_samples_path))
-        center_naive(args.organs_dir_path, train_samples, test_samples, args.mode)
+        frequency_naive(args.organs_dir_path, train_samples, test_samples, args.mode)
 
 
 def parse_args():
@@ -173,7 +146,7 @@ def parse_args():
         "--mode",
         "-m",
         type=str,
-        help='Mode - one of {"center", "random_easy", "random_hard"}, setting for naive evaluation',
+        help='Mode - one of {"frequency", "random"}, setting for naive evaluation',
     )
     parser.add_argument(
         "--bbox-shrink",
