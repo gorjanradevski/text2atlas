342d9705f15f5631a45d3307aec3f533be1ec7a9
dusangrujicic <dusangr22@gmail.com>
2019-12-08 12:46:18 +0100

    distance metric

--- src/voxel_mapping/evaluator.py
+++ src/voxel_mapping/evaluator.py
@@ -10,6 +10,127 @@ import torch
 from utils.constants import VOXELMAN_CENTER
 
 
+class InferenceEvaluator:
+    def __init__(
+        self, ind2organ_path: str, organ2label_path: str, organ2summary_path: str, voxelman_images_path: str
+    ):
+        self.organ2label = json.load(open(organ2label_path))
+        self.ind2organ = json.load(open(ind2organ_path))
+        self.organ2summary = json.load(open(organ2summary_path))
+        image_files = natsort.natsorted(
+            [
+                os.path.join(voxelman_images_path, f)
+                for f in os.listdir(voxelman_images_path)
+                if os.path.isfile(os.path.join(voxelman_images_path, f))
+                and f.endswith(".tif")
+            ]
+        )[::-1]
+        self.voxelman = tifffile.imread(image_files).transpose(1, 2, 0)
+        self.best_avg_accuracy = -1
+        self.current_average_accuracy = 0
+        self.total = 0
+        self.correct = 0
+        self.distance = 0.0
+
+    def reset_current_average_accuracy(self):
+        self.current_average_accuracy = 0
+
+    def reset_counters(self):
+        self.total = 0
+        self.correct = 0
+        self.distance = 0.0
+
+    def update_counters(self, output_mapping: np.ndarray, organ_indices: np.ndarray):
+        self.total += 1
+        self.correct += self.voxels_inside(output_mapping, organ_indices)
+        self.distance += self.voxels_distance(output_mapping, organ_indices)
+
+    def voxels_inside(
+        self, pred: np.ndarray, organ_indices: Union[List, np.ndarray]
+    ) -> int:
+        """
+        :param pred: prediction for one sample, numpy array or shape (3,)
+        :param organ_indices: set of true organ indices for the sample, list or numpy array
+        :return: Array with k entries
+         1.0 at i-th entry - pred is inside the voxels of the i-th organ,
+         0.0 at i-th entry - pred is outside of the voxels of the i-th organ
+        """
+        organ_indices = np.array(organ_indices)
+        corrects = np.zeros(organ_indices.size)
+        pred = np.round(pred + VOXELMAN_CENTER)
+        pred = np.clip(
+            pred, a_min=np.array([0, 0, 0]), a_max=(np.array(VOXELMAN_CENTER) * 2 - 1)
+        )
+        for i, organ_index in enumerate(organ_indices):
+            labels = self.organ2label[self.ind2organ[str(organ_index)]]
+            x, y, z = pred.astype(int)
+            corrects[i] = int(self.voxelman[x, y, z] in labels)
+
+        return 1 if np.count_nonzero(corrects) > 0 else 0
+
+    def voxels_distance(
+        self, pred: np.ndarray, organ_indices: Union[List, np.ndarray]
+    ) -> np.ndarray:
+        """
+        :param pred: prediction for one sample, numpy array or shape (3,)
+        :param organ_indices: set of true organ indices for the sample, list or numpy array
+        :return: Array with k entries
+         x at i-th entry - pred is x away from nearset voxel of the i-th organ,
+         0.0 at i-th entry - pred is inside of the voxels of the i-th organ
+        """
+        organ_indices = np.array(organ_indices)
+        distances = np.zeros(organ_indices.size)
+        pred_ind = np.round(pred + VOXELMAN_CENTER)
+        pred_ind = np.clip(
+            pred_ind, a_min=np.array([0, 0, 0]), a_max=(np.array(VOXELMAN_CENTER) * 2 - 1)
+        )
+        for i, organ_index in enumerate(organ_indices):
+            labels = self.organ2label[self.ind2organ[str(organ_index)]]
+            x, y, z = pred_ind.astype(int)
+            inside = int(self.voxelman[x, y, z] in labels)
+            if inside:
+                distances[i] = 0.0
+            else:
+                summary_points = np.array(self.organ2summary[self.ind2organ[str(organ_index)]])
+                distances[i] = np.sqrt(np.square(pred - summary_points).sum(axis=1)).min()
+
+        return distances.min()
+
+    def get_current_accuracy(self):
+        return self.correct / self.total
+
+    def get_current_distance(self):
+        return self.distance / self.total
+
+    def update_current_average_accuracy(self):
+        self.current_average_accuracy += self.correct / self.total
+
+    def finalize_current_average_accuracy(self):
+        self.current_average_accuracy /= 2
+
+    def is_best_avg_accuracy(self):
+        return self.current_average_accuracy > self.best_avg_accuracy
+
+    def update_best_avg_accuracy(self):
+        self.best_avg_accuracy = self.current_average_accuracy
+
+    def bbox_inside(pred: np.ndarray, bboxes: np.ndarray):
+        """
+        Return
+        :param pred: prediction for one sample, shape (3,)
+        :param bboxes: set of bounding boxes of k organs inside the sample sentence, shape (k, 3, 2)
+        :return: Array with k entries
+        1.0 at i-th entry - pred is inside the i-th bounding box,
+        0.0 at i-th entry - prediction is outside of the i-th bounding box
+        """
+        corrects = (
+            np.concatenate((pred - bboxes[:, :, 0], bboxes[:, :, 1] - pred), axis=-1)
+            >= 0
+        ).all(axis=-1)
+
+        return 1 if np.count_nonzero(corrects) > 0 else 0
+
+
 class Evaluator:
     def __init__(
         self, ind2organ_path: str, organ2label_path: str, voxelman_images_path: str
@@ -43,7 +164,7 @@ class Evaluator:
 
     def voxels_inside(
         self, pred: np.ndarray, organ_indices: Union[List, np.ndarray]
-    ) -> np.ndarray:
+    ) -> int:
         """
         :param pred: prediction for one sample, numpy array or shape (3,)
         :param organ_indices: set of true organ indices for the sample, list or numpy array
