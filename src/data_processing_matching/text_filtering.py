0e6127446e409c43ccffdeea8510e2f044f95a6d
gorjanradevski <gorjanradevski@gmail.com>
2020-04-01 10:51:50 +0200

    cleaning up

--- src/data_processing_matching/text_filtering.py
+++
@@ -1,9 +0,0 @@
-import re
-
-
-def process_caption(text: str) -> str:
-    text = re.sub(r"[\s\n]", " ", text)
-    text = re.sub(r"[ \t]{2,}", " ", text)
-    text = re.sub(r"[Ff]ig\s*\.*\s*\d+\.*", " ", text)
-    text = text.strip()
-    return text
