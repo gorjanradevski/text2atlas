6f77b336772f79bdcfada8d6c0d0f63a217e06b9
Gorjan Radevski <gorjanradevski@gmail.com>
2020-04-13 11:37:04 +0200

    inclided the model cards

--- src/voxel_mapping/models.py
+++ src/voxel_mapping/models.py
@@ -10,10 +10,10 @@ logger = logging.getLogger(__name__)
 
 class SentenceMappingsProducer(nn.Module):
     def __init__(self, bert_name: str, config: BertConfig, final_project_size: int):
-        # emilyalsentzer/Bio_ClinicalBERT (https://huggingface.co/emilyalsentzer/Bio_ClinicalBERT)
-        # SciBERT
-        # BioBERT
-        # google/bert_uncased_L-2_H-128_A-2 (https://huggingface.co/google)
+        # https://huggingface.co/emilyalsentzer/Bio_ClinicalBERT)
+        # https://huggingface.co/allenai/scibert_scivocab_uncased
+        # https://huggingface.co/monologg/biobert_v1.1_pubmed
+        # https://huggingface.co/google
         super(SentenceMappingsProducer, self).__init__()
         self.bert = BertModel.from_pretrained(bert_name)
         config.vocab_size = final_project_size
